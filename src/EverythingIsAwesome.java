69ab19edfa034199131319068801f0019228aecd
Mrfuzzy <league@WTS9.attlocal.net>
2016-01-15 18:20:44 -0800

    class 2

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+String awesome =	JOptionPane.showInputDialog("whats awesome");
+	JOptionPane.showMessageDialog(null,awesome+ " is awesome!" );
+	
+}
+}
--- src/RobotE.java
+++ src/RobotE.java
@@ -12,7 +12,12 @@ E.move(100);
 E.penUp();
 E.turn(90);
 E.move(100);
-E.penDown();
+E.penUp();
 E.turn(90);
+E.penDown();
+E.move(90);
+E.turn(-90);
+E.penDown();
+E.move(distance);
 }
 }
