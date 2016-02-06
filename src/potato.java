26cf860b679737a80e8dbce1b83a94bc1c138d69
Mrfuzzy <league@WTS9.attlocal.net>
2016-02-05 18:33:39 -0800

    hi

---
+++ src/potato.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class potato {
+public static void main(String[] args) {
+	for(int i=1 ; i<8; i++){
+		JOptionPane.showMessageDialog(null,i+"potato");
+		 if(i==7)JOptionPane.showMessageDialog(null, "more");
+	}}}
+
+
