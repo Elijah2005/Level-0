2c4b86c5f645727d40f9c3ddadebd22cb1836b4d
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-04-22 18:26:20 -0700

    hi

---
+++ src/IKnowWhatYouDidLastSummer.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class IKnowWhatYouDidLastSummer {public static void main(String[] args) {
+	
+String nothing =JOptionPane.showInputDialog("What did you do last summer ");
+	String summer=JOptionPane.showInputDialog("Whats your name");
+	 JOptionPane.showMessageDialog(null,"Hello " +summer+ "I know what you did last summer "+ nothing);
+	
+	
+}
+
+}
