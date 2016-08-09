2b26c25d48544cfa8ced5b9c8801d25dfc08beb8
Love4653 <Kaiya.park@gmail.com>
2016-08-08 18:22:02 -0700

    hi

--- src/riddleMeThis.java
+++ src/riddleMeThis.java
@@ -9,10 +9,17 @@ int number = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/the-riddler
 String a= JOptionPane.showInputDialog("You draw a line. Without touching it, how do you make the line longer");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-JOptionPane.showMessageDialog(null, "Correct");
-number ++;
-		// 5. Otherwise, say "wrong" and tell them the answer
+		
 
+ JOptionPane.showMessageDialog( null, " you have to draw another line next to it without touching the other line");
+ if(a.equals("You have to draw another line next to it without touching the other line")){
+	JOptionPane.showMessageDialog(null, "correct");
+	number++;
+	
+ }else{
+	 JOptionPane.showMessageDialog(null, "Wrong");
+	 number--;
+ }
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
