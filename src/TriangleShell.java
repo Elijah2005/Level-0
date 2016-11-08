87ec8754e1daf64a79b390820cab6d313485eb1f
Garrett-Thomas <league@WTS10.attlocal.net>
2016-11-07 18:25:17 -0800

    hi

---
+++ src/TriangleShell.java
@@ -0,0 +1,79 @@
+import org.jointheleague.graphical.robot.Robot;
+
+
+public class TriangleShell {
+	int length = 180;
+
+	// 1. Create a new Robot
+Robot elijah = new Robot();
+void go (){
+
+	
+	 // 3. delete this line (used only for testing)
+
+
+		// 6. Make the robot go as fast as possible
+elijah.setSpeed(10);
+
+		// 4. make a variable to hold the length of the triangle and set it to 50
+
+
+		// 7. Do the following (up to step 10) 60 times
+for (int i = 0; i < 60; i++) {
+	
+
+
+			// 9. Change the color of the pen to a random color
+elijah.setRandomPenColor();
+	
+			// 8. Increase the length of the side by 10 pixels
+	length+=10;
+			// 5. call your drawTriangle() method using your length variable
+	 drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+	 elijah.turn(6);
+}
+}
+
+	
+
+
+	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
+	private void drawTriangle(int length) {
+		
+			
+		
+		elijah.penDown();
+		elijah.move(length);
+		elijah.turn(120);
+		elijah.move(length);
+		elijah.turn(120);
+		elijah.move(length);
+		elijah.turn(120);
+		
+	}
+
+
+	
+	public static void main(String[] args) {
+		new TriangleShell().go();
+		
+	}
+
+
+
+	
+		
+		// TODO Auto-generated method stub
+		
+	
+
+}
+
+
+
+
+
+
+
+
