a5952183f084c05373d55452564bd07fafdd3558
Love4653 <league@WTS5.attlocal.net>
2016-11-28 18:26:02 -0800

    hi

---
+++ src/ForLoopGauntlet2.java
@@ -0,0 +1,29 @@
+
+public class ForLoopGauntlet2 {
+public static void main(String[] args) {
+	//for (int i = 0; i < 101; i++) {	
+	//System.out.println(i);
+//	}
+//for (int i = 100; i > -1; i--) {
+//	System.out.println(i);
+	
+//}
+//	for (int i = 2; i < 101; i++) {
+	//	System.out.println(i);
+		
+	//}
+	//for (int i = 1 ; i < 100; i+=2) {
+	//	System.out.println(i);
+	//}	
+	for (int i = 1; i < 500; i++) {
+		if((i/3)==0){
+			System.out.println(i+(" divisible by 3"));
+		}else{
+			System.out.println(i+(" not divisible by 3"));
+		}
+	}
+
+}
+}
+
+
